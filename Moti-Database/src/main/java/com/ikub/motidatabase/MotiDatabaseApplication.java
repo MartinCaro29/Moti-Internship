e8f04b24c6d5f658c9734a04663a85b081e7c73f
MartinCaro29 <168284552+MartinCaro29@users.noreply.github.com>
2024-04-27 16:37:55 +0200

    Add files via upload

---
+++ Moti-Database/src/main/java/com/ikub/motidatabase/MotiDatabaseApplication.java
@@ -0,0 +1,15 @@
+package com.ikub.motidatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotiDatabaseApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(MotiDatabaseApplication.class, args);
+	}
+
+}
